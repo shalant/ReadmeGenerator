caf6fdc787e082c20f1725c40a5c7005b61b1b26
Doug Rosenberg <doug.rosenberg@gmail.com>
2020-08-28 14:04:48 -0500

    added questions

---
+++ index.js
@@ -0,0 +1,97 @@
+// array of questions for user
+const questions = [
+
+];
+
+
+//https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: 'Title',
+      message: 'What is the title of your project?',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Description',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Table of Contents',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Installation',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Usage',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'License',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Contributing',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+     {
+      type: "input",
+      name: 'License',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Tests',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+    {
+      type: "input",
+      name: 'Questions',
+      message: 'Please describe the content',
+      default: 'Do better!',
+    },
+  ])
+  .then(answers => {
+    console.info('Answers:', answers);
+  });
+
+  //from bcs tutor
+  const questions = [
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    }
+
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
