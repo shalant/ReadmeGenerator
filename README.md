d31f187a42c2eec9a05ae4efa395d2a719e860b0
Doug Rosenberg <doug.rosenberg@gmail.com>
2020-08-31 09:37:05 -0500

    minor formatting

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
-# mama
-    ## 'Description:'
-    ### does stuff
-    ## 'Table of Contents:'
-    ### '    1. Installation'(#Installation)
-    ### '    2. Usage'(#Usage)
-    ### '    3. License'(#License)
-    ### '    4. Contributing'(#Contributing)
-    ### '    5. Testing'(#Tests)
-    ### '    6. Questions'(#Questions)
+# blah
+    ## Description:
+    ### blah
+    
+    ## Table of Contents:
+    ###     *[1. Installation](#Installation)
+    ###     *[2. Usage](#Usage)
+    ###     *[3. License](#License)
+    ###     *[4. Contributing](#Contributing)
+    ###     *[5. Testing](#Tests)
+    ###     *[6. Questions](#Questions)
 
-    ### '1. Installation'
-    #### do this
+    ### 1. Installation
+    #### blah
 
-    ### '2. Usage'
-    #### for food
+    ### 2. Usage
+    #### blah
 
-    ### '3. License'
+    ### 3. License
     #### 
 
-    ### '4. Contributing'
-    #### joe
+    ### 4. Contributing
+    #### blah
 
-    ### '5. Tests'
-    #### yes
+    ### 5. Tests
+    #### blha
     
-    ### '6. Questions'
-    #### With any questions send an e-mail to doug@doug.com
-    #### Visit https://github.com/alanturing
+    ### 6. Questions
+    #### With any questions send an e-mail to blah@blah.com
+    #### Visit https://github.com/bluah
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,33 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-    ## 'Description:'
+    ## Description:
     ### ${data.description}
-    ## 'Table of Contents:'
-    ### '    1. Installation'(#Installation)
-    ### '    2. Usage'(#Usage)
-    ### '    3. License'(#License)
-    ### '    4. Contributing'(#Contributing)
-    ### '    5. Testing'(#Tests)
-    ### '    6. Questions'(#Questions)
+    
+    ## Table of Contents:
+    ###     *[1. Installation](#Installation)
+    ###     *[2. Usage](#Usage)
+    ###     *[3. License](#License)
+    ###     *[4. Contributing](#Contributing)
+    ###     *[5. Testing](#Tests)
+    ###     *[6. Questions](#Questions)
 
-    ### '1. Installation'
+    ### 1. Installation
     #### ${data.installation}
 
-    ### '2. Usage'
+    ### 2. Usage
     #### ${data.usage}
 
-    ### '3. License'
+    ### 3. License
     #### ${data.license}
 
-    ### '4. Contributing'
+    ### 4. Contributing
     #### ${data.contributing}
 
-    ### '5. Tests'
+    ### 5. Tests
     #### ${data.tests}
     
-    ### '6. Questions'
+    ### 6. Questions
     #### With any questions send an e-mail to ${data.questions}
     #### Visit https://github.com/${data.githubUsername}
 
